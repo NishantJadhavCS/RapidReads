d945fde34411e58b0de33ae9a14b6642d3fa6277
Nishant Jadhav <nishantjadhav.cs@gmail.com>
2024-04-02 22:20:26 +0530

    commit undo

---
+++ static/js/home.js
@@ -0,0 +1,9 @@
+
+AOS.init();
+
+const learnMoreButton = document.querySelector('.learn-more');
+const nextSection = document.querySelector('#feed-info');
+
+learnMoreButton.addEventListener('click', () => {
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
